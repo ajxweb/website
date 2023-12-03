db80e35800e70e590654f6ca391267260ec30a32
REDROOMIE <142141092+redroomie@users.noreply.github.com>
2023-12-03 17:29:09 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aiden's Fucking Website
+
+### Welcome to Aiden's Fucking Website!
+
+This is my original website I made after I started using the dark web. It is being continuously updated, so check back soon ;P
+
+Link: https://aidenx.gitlab.io/site/
---
+++ pages/output.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>
+</title>
+</head>
+<body>
+
+<?php
+
+	$name = $_POST["name"];
+	echo "Hello, " . $name . "." . "<br>";
+	echo $name . " is the sexiest name I've ever heard, holy fucking shit!!!";
+
+?>
+
+<p><a href="test.php">Try it again!</a></p>
+<p><a href="../.">This is retarded, take me back.</a></p>
+</body>
+</html>
